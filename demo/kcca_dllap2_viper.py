2602874d7f6f26e9840ae5645302a2cc09de01df
muggledy <3101266674@qq.com>
2020-07-22 23:41:24 +0800

    delete udlgl

--- demo/kcca_dllap2_viper.py
+++ demo/kcca_dllap2_viper.py
@@ -1,69 +1,120 @@
 '''
-This work is previous of udlgl by author Elyor Kodirov, we use the same data as 
-./demo_laplacianl1_kcca_viper.py. Because of the complexity of it's optimization part(see paper 
-"Graph Regularized Sparse Coding for Image Representation"), i.e. feature sign search,  so i 
-modify the objective function(||Y||_1=>||Y||_F) accroding to my boss's guidance
+This method(unsupervised dictionary learning with graph laplacian L2 regularization) use features 
+in [2], you can download from [3](VIPeR_split.mat), then put in ../data/, test with XQDA we 
+get rank-1 34.15%(average after 10 iters)
+Because of the complexity of it's optimization part(see paper 
+"Graph Regularized Sparse Coding for Image Representation"), i.e. feature sign search, so i 
+modify the objective function(||Y||_1=>||Y||_F) accroding to my teacher's guidance
 Issue:
 Paper[1] claims that rank-1 of viper can achieve 30%(cam_b as probe), but in fact there is a 
-flaw in the public source code, only has 23.x% if you shuffle the training data firstly(in matlab), 
+flaw in the public source code, only has 23% if you shuffle the training data (test in matlab, 
+but i got 25% with python code), 
 the problem lies in the construction of the affinity matrix W(see graph_cross_view.m in source code)
+see author's web page: https://sites.google.com/site/elyorkodirovresearch/publications
+2020/5/21
 References:
 [1] Kodirov, Elyor & Xiang, Tao & Gong, Shaogang. (2015). Dictionary Learning with Iterative 
-    Laplacian Regularisation for Unsupervised Person Re-identification. 44.1-44.12. 10.5244/C.29.44. 
+    Laplacian Regularisation for Unsupervised Person Re-identification. 44.1-44.12. 10.5244/C.29.44.
+[2] Giuseppe Lisanti et al., Matching People across Camera Views using Kernel Canonical Correlation 
+    Analysis”, Eighth ACM/IEEE International Conference on Distributed Smart Cameras, 2014.
+[3] https://github.com/glisanti/KCCAReId
 '''
 
 from initial import *
-from kcca_laplacianl1_viper import get_data
-from zoo.udlgl import get_cross_view_graph
+from lomo.xqda import xqda
+from zoo.dllap import get_cross_view_graph,dllap
 from zoo.optimize import opt_coding_l2
 from zoo.tools import norm_labels_simultaneously
-from zoo.dllap import dllap
+import scipy.io as sio
 
-k_nn=3
-nBasis=2**8
-alpha=1.0
-beta=0.0001
-nIters=50
-lambd1=0.04 #this parametr must be tuned
+def get_data(trial=0,dataset='viper'):
+    file_path=os.path.join(os.path.dirname(__file__),'../data/%s_split.mat'%dataset)
+    data=sio.loadmat(file_path)['trials'][0]
+    s='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    trial_num=len(data)
 
-cs=[]
-D=None
-for trial in range(1):
-    print('Trial-%d...'%(trial+1))
-    data=get_data(trial)
-    probFea1,galFea1=data['trainA_feats'],data['trainB_feats']
-    # probFea1,galFea1=probFea1[:,np.random.permutation(probFea1.shape[1])], \
-    #     galFea1[:,np.random.permutation(galFea1.shape[1])] #verify: shuffle samples' order won't 
-                                                           #affect result if don't use backdoor 
-                                                           #in func get_cross_view_graph, this result 
-                                                           #is reliable
-    probFea2,galFea2=data['testA_feats'],data['testB_feats']
-    
-    for in_iter in range(1):
-        print('1.construct graph(W) similarity matrix...')
-        if in_iter==0:
-            W_full=get_cross_view_graph(probFea1,galFea1,k=k_nn,backdoor=True)
-        elif in_iter==1:
-            print('use the learned samples\' coding this time')
-            W_full=get_cross_view_graph(train_a_after_D,train_b_after_D,k=k_nn,backdoor=True)
+    if trial<0 or trial>=trial_num:
+        raise ValueError('trial must be in [0,%d]!'%(trial_num-1))
+    data_trial=data[trial]
+    inds=list(zip(range(0,len(data_trial),2),range(1,len(data_trial),2)))
+    test_inds=inds[:len(inds)//2]
+    train_inds=inds[len(inds)//2:]
+    ret={}
+    for i,(labels,feats) in enumerate(train_inds):
+        cam=s[i]
+        ret['train%s_labels'%cam]=data_trial[labels]
+        ret['train%s_feats'%cam]=data_trial[feats]
+    for i,(labels,feats) in enumerate(test_inds):
+        cam=s[i]
+        ret['test%s_labels'%cam]=data_trial[labels]
+        ret['test%s_feats'%cam]=data_trial[feats]
+    return ret
 
-        print('2.learn dictionary(D) with L2 laplacian...')
-        D=dllap(np.hstack((probFea1,galFea1)), W_full, nBasis, alpha, beta, nIters, Dinit=D)
+### test of kcca descriptors + XQDA
+# cs=[]
+# for trial in range(0,10):
+#     data=get_data(trial)
+#     probFea1,galFea1=data['trainA_feats'],data['trainB_feats']
+#     probFea2,galFea2=data['testA_feats'],data['testB_feats']
 
-        train_a_after_D,P=opt_coding_l2(D,probFea1,lambd1)
-        train_b_after_D=P.dot(galFea1)
+#     W,M,*_=xqda(probFea1,galFea1,*norm_labels_simultaneously(data['trainA_labels'][0],data['trainB_labels'][0]))
+#     dist=mah_dist(M,(W.T).dot(probFea2),(W.T).dot(galFea2))
+#     c=calc_cmc(dist.T,*norm_labels_simultaneously(data['testA_labels'][0],data['testB_labels'][0]),100)
+#     cs.append(c)
 
-    test_a_after_D,P=opt_coding_l2(D,probFea2,lambd1)
-    test_b_after_D=P.dot(galFea2)
-    dist=cosine_dist(test_a_after_D,test_b_after_D)
-    print('calc CMC...')
-    c=calc_cmc(dist,*norm_labels_simultaneously(data['testB_labels'][0],data['testA_labels'][0]),100) #
-                                                       #note here: cam_b as probe, if cam_a as probe, 
-                                                       #rank scores will decrease a little
-    cs.append(c)
-    print_cmc(c)
+# c_mean=np.mean(cs,axis=0)
+# print('CMC:')
+# print(c_mean)
+# plot_cmc(c_mean,['viper'],verbose=True)
+###
 
-print('average CMC:')
-c_mean=np.mean(np.array(cs),axis=0)
-print_cmc(c_mean,color=True)
-plot_cmc(c_mean,['viper'],verbose=True)
+if __name__=='__main__':
+    k_nn=3
+    nBasis=2**8
+    alpha=1.0
+    beta=0.0001
+    nIters=50
+    lambd1=0.04
+
+    cs=[]
+    D=None
+    for trial in range(1):
+        print('Trial-%d...'%(trial+1))
+        data=get_data(trial)
+        probFea1,galFea1=data['trainA_feats'],data['trainB_feats']
+        probFea1,galFea1=probFea1[:,np.random.permutation(probFea1.shape[1])], \
+            galFea1[:,np.random.permutation(galFea1.shape[1])] #verify: shuffle samples' order won't 
+                                                               #affect result if don't use backdoor 
+                                                               #in func get_cross_view_graph, this result 
+                                                               #is reliable
+        probFea2,galFea2=data['testA_feats'],data['testB_feats']
+        
+        for in_iter in range(1):
+            print('1.construct graph(W) similarity matrix...')
+            if in_iter==0:
+                W_full=get_cross_view_graph(probFea1,galFea1,k=k_nn,backdoor=False)
+            elif in_iter==1:
+                print('use the learned samples\' coding this time')
+                W_full=get_cross_view_graph(train_a_after_D,train_b_after_D,k=k_nn,backdoor=False)
+
+            print('2.learn dictionary(D) with L2 laplacian...')
+            D=dllap(np.hstack((probFea1,galFea1)), W_full, nBasis, alpha, beta, nIters, Dinit=D) #
+                                                        #非常奇怪的是，有时候收敛，有时候不收敛
+
+            train_a_after_D,P=opt_coding_l2(D,probFea1,lambd1)
+            train_b_after_D=P.dot(galFea1)
+
+        test_a_after_D,P=opt_coding_l2(D,probFea2,lambd1)
+        test_b_after_D=P.dot(galFea2)
+        dist=cosine_dist(test_a_after_D,test_b_after_D)
+        print('calc CMC...')
+        c=calc_cmc(dist,*norm_labels_simultaneously(data['testB_labels'][0],data['testA_labels'][0]),100) #
+                                                        #note here: cam_b as probe, if cam_a as probe, 
+                                                        #rank scores will decrease a little
+        cs.append(c)
+        print_cmc(c)
+
+    print('average CMC:')
+    c_mean=np.mean(np.array(cs),axis=0)
+    print_cmc(c_mean,color=True)
+    plot_cmc(c_mean,['viper'],verbose=True)
--- src/zoo/dllap.py
+++ src/zoo/dllap.py
@@ -1,5 +1,5 @@
 '''
-See demo in ../demo_laplacianl2_kcca_viper.py
+See demo in ../../demo/kcca_dllap2_viper.py
 muggledy 2020/6/7
 '''
 
@@ -10,7 +10,41 @@ import sys
 sys.path.append(os.path.join(os.path.dirname(__file__),'../'))
 from zoo.graph_sc import learn_dict
 from itertools import count
+from zoo.tools import sym_matric,cosine_dist
 from zoo.cprint import cprint_err,cprint_out
+
+def max_knn(D,k):
+    '''salient D's maximum k elements according to axis 1(horizon)'''
+    ret=np.zeros(D.shape)
+    sort_inds=np.argsort(D,axis=1)[:,::-1]
+    sal_inds=(np.broadcast_to(np.arange(D.shape[0])[:,None],(D.shape[0],k)),sort_inds[:,:k])
+    ret[sal_inds]=D[sal_inds]
+    return ret
+
+def get_cross_view_graph(*X,k=5,**kwargs):
+    '''construct soft cross-view correspondence relationship matric W, more precisely, if x_i is 
+       among the k-nearest neighbours of y_j or vice versa, W_{i,j}=cosine_dist(x_i,y_j), else 
+       w_{i,j}=0. X is a set, each of which represents all samples coming from one camera'''
+    n=len(X)
+    y,x=np.meshgrid(range(n),range(n))
+    inds=np.triu_indices(n,1)
+    x,y=x[inds],y[inds]
+    t=np.cumsum([0]+[i.shape[1] for i in X])
+    W=np.zeros((t[-1],t[-1])) #we don't consider intra-view relationship, just set them as zero
+    for i,j in zip(x,y):
+       simi=1-cosine_dist(X[i],X[j])
+       d2=max_knn(simi.T,k)
+       if kwargs.get('backdoor'):
+           print('use backdoor!')
+           d1=d2 #same as source code of laplacianL2, but it is 
+                 #not reasonable and may cause shape mismatch error!
+       else:
+           d1=max_knn(simi,k) #this(I) should be right!
+       W[t[i]:t[i+1],t[j]:t[j+1]]=(d1+d2.T)/2
+    W=sym_matric(W)
+    W[(W<0.5)&(W!=0)]=0.0001 #remove "noise" from the graph
+    W[range(W.shape[0]),range(W.shape[0])]=0
+    return W
 
 def opt_lap_Y(X,D,L,alpha,beta):
     '''min ||X-DY||_F^2 + alpha||Y||_F^2 + beta*tr(YLYᵀ)'''
